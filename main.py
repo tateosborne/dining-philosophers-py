e33afdbe0250de3ae73419b8e5481d6d73207be5
tateosborne <tate0301@gmail.com>
2022-08-28 18:03:36 -0400

    implemented part of eating sequence

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
+from pickle import TRUE
 import threading
 
 class Philosopher(threading.Thread):
+  # boolean field which is flagged true until all Philosophers have eaten
+  complete = False
   
   def __init__(self, idx, forkL, forkR, eaten) -> None:
-    threading.Thread.__init__(self)
     self.idx = idx
     self.forkL = forkL
     self.forkR = forkR
-    self.eaten = eaten
-      
+    threading.Thread.__init__(self)
+    
+  def begin(self):
+    while not self.complete:
+      self.dinner_time()
       
   def dinner_time(self):
-    if self.eaten:
-      return
-    else:
-      while self.forkL and self.forkR == True:
-        self.
+    forkleft = self.forkL
+    forkright = self.forkR
+    while not self.complete:
+      pass
         
 def main():
-  forks = [threading.Semaphore() for n in range(8)]
-  philosophers = [Philosopher(i, forks[i % 8], forks[i+1 % 8], False) for i in range(8)]
+  philosophers = []
+  forks = []
+  for i in range(5):
+    forks[i] = threading.Semaphore
+  for i in range(5):
+    philosophers[i] = Philosopher(i, forks[i%5], forks[(i+1)%5])
   
-  Philosopher.running = True
-  for p in philosophers[p]:
-    p.dinner_time()
- 
+  for phil in philosophers:
+    phil.begin()
  
 if __name__ == "__main__":
   main()
-  
